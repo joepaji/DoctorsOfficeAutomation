1a541bfc817ee34cce33d8369a51cf9ae6899e33
Joheb R <rjoheb@hotmail.com>
2021-11-03 12:30:17 -0700

    created and tested database class. This creates database object and has connect method

--- src/application/Database.java
+++ src/application/Database.java
@@ -2,39 +2,26 @@ package application;
 import java.sql.*;
 
 public class Database {
+	private String url = "jdbc:postgresql://ec2-3-217-91-165.compute-1.amazonaws.com:5432/daru65ongv34rq";
+	private String username = "vabxlxvlcddmrq";
+	private String password = "972f4aeaf501184937bbc4680ac80512f01726446c3ffd7841a4a8bb449ae6da";
 	
-	public static void main(String[] args) {
+	
+	// Creates a database connection to the project database. Remember to invoke .close() to close the connection after use
+	public Connection connect() {
 		
-		//THIS NEEDS TO CHANGE AND COME FROM OUR ACTUAL DATABASE
-		String url = "jdbc:postgresql://ec2-3-217-91-165.compute-1.amazonaws.com:5432/daru65ongv34rq";
-		String username = "vabxlxvlcddmrq";
-		String password = "972f4aeaf501184937bbc4680ac80512f01726446c3ffd7841a4a8bb449ae6da";
-	
 		try {
-			
 			Connection c = DriverManager.getConnection(url,username,password);
-			
 			System.out.println("Connected to server.");
 			
-			String sql = "SELECT * FROM doctors";
-					
-			Statement stmt = c.createStatement();
-
-			ResultSet result = stmt.executeQuery(sql);
-			
-			while(result.next()) {
-				String user = result.getString("username");
-				String firstName = result.getString("first_name");
-				String lastName = result.getString("last_name");
-				
-				System.out.println("Username: "+user+"\nFirst Name: "+firstName+"\nLast Name: "+lastName);
-			}
-			
-			c.close();
-		} catch (SQLException e) {
+			return c;
+		}
+		catch (SQLException e) {
 			System.out.println("Error in connecting to postgreSQL server.");
 			e.printStackTrace();
 		}
 		
-	}  
+		return null;
+	}
+
 }
