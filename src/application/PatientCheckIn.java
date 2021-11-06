6686d2dbc40ad23f7e085dd312dee663a788ed40
MacBook Pro <macpro@MacBooks-MacBook-Pro.local>
2021-11-05 20:22:19 -0700

    built controller for patient check-in and connected it to the fxml

--- src/application/PatientCheckIn.java
+++ src/application/PatientCheckIn.java
@@ -1,5 +1,112 @@
 package application;
 
-public class PatientCheckIn {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
+public class PatientCheckIn{
+	
+	@FXML
+	private Button signOut;
+	@FXML
+	private Button messages;
+	@FXML
+	private Button home;
+	@FXML
+	private Button confirmEntry;
+	@FXML
+	private TextField height;
+	@FXML
+	private TextField weight;
+	@FXML
+	private TextField temperature;
+	@FXML
+	private TextField bloodPressure;
+	@FXML
+	private TextArea healthConcerns;
+	@FXML
+	private TextArea medications;
+	@FXML
+	private TextArea allergies;
+	@FXML
+	private TextArea notes;
+	
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private String destination;
+	private String heightData;
+	private String weightData;
+	private String temperatureData;
+	private String bpData;
+	private String healthConcernsData;
+	private String medicationData;
+	private String allergiesData;
+	private String notesData;
+	
+	
+	public PatientCheckIn()
+	{
+		
+	}
+	
+	public void signOut(ActionEvent event) throws IOException
+	{	
+		destination = "login.fxml";
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void toMessages(ActionEvent event) throws IOException
+	{
+		//destination = "Messages.fxml";
+		destination = "login.fxml";    //testing
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void toHome(ActionEvent event) throws IOException
+	{
+		destination = "login.fxml"; 
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void confirmEntry(ActionEvent event) throws IOException
+	{
+		heightData = height.getText().toString();
+		weightData = weight.getText().toString();
+		temperatureData = temperature.getText().toString();
+		bpData = height.getText().toString();
+		healthConcernsData = weight.getText().toString();
+		medicationData = temperature.getText().toString();
+		allergiesData = height.getText().toString();
+		notesData = weight.getText().toString();
+		
+		
+		// test
+		System.out.println(heightData);
+		System.out.println(weightData);
+		
+	}
+	
 }
