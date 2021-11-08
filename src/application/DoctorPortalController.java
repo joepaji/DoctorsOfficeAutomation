3f8dcd866ce1cc07ee5342b6badb40df9dad08c3
kase lunt <kllunt@asu.edu>
2021-11-08 15:46:28 -0700

    finished doctorPortalController fxml and got the buttons all connected to the appropriate functions. Still needs toMessages

--- src/application/DoctorPortalController.java
+++ src/application/DoctorPortalController.java
@@ -6,10 +6,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 
@@ -20,9 +23,9 @@ public class DoctorPortalController{
 	@FXML
 	private Button toMessages;
 	@FXML
-	private Button toHome;
+	private Button home;
 	@FXML
-	private Button checkIn;
+	private Button beginExam;
 	@FXML
 	private Button search;
 	@FXML
@@ -31,11 +34,18 @@ public class DoctorPortalController{
 	private TextField lastName;
 	@FXML
 	private TextField dateOfBirth;
+	@FXML
+	private Label searchError;
+	@FXML
+	private Label examError;
+	@FXML
+	private TextArea checkInSummary;
 	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	private String destination;
+	private String username;
 	
 	public DoctorPortalController()
 	{
@@ -66,6 +76,34 @@ public class DoctorPortalController{
 	public void search(ActionEvent event) throws IOException
 	{
 		System.out.println("search");
+		try {
+			Database db = new Database();
+			Connection c = db.connect();
+			Statement stmt = c.createStatement();
+			
+			// search the database for shopping_cart with matching userID
+			String sql = "SELECT * FROM patient WHERE (first_name = '" + firstName.getText().toString() +
+												  "' AND last_name = '" + lastName.getText().toString() + "'"
+												  		+ "AND dob = '" + dateOfBirth.getText().toString()+"');";	
+			ResultSet rs = stmt.executeQuery(sql); 
+			
+			if(rs.next()) {
+				String first = rs.getString("first_name");
+				String last = rs.getString("last_name"); 
+				username = rs.getString("username");
+				// ************** TODO
+				System.out.println(first);
+				System.out.println(last);
+				searchError.setText("");
+				displayLastCheckin();
+			} else {
+				searchError.setText("Patient not found.");
+			}
+				
+		} catch (SQLException e) {	
+			System.out.println("Error in connecting to postgreSQL server.");
+			e.printStackTrace();
+		}
 	}
 	
 	public void beginExam(ActionEvent event) throws IOException
@@ -77,6 +115,10 @@ public class DoctorPortalController{
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+	}
+	public void displayLastCheckin() {
+		DoctorNurseActions actions = new DoctorNurseActions(username);
+		checkInSummary.setText(actions.getLatestCheckin());
 	}
 	
 }
