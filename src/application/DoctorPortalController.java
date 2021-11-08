8904071e30a974ce368e782813ea8c49adb1c2fc
Devin Merritt <djmerri1@asu.edu>
2021-11-08 16:33:55 -0700

    changes to windows and doctor portal

--- src/application/DoctorPortalController.java
+++ src/application/DoctorPortalController.java
@@ -6,10 +6,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 
@@ -31,11 +34,18 @@ public class DoctorPortalController{
 	private TextField lastName;
 	@FXML
 	private TextField dateOfBirth;
+	@FXML
+	private Label searchError;
+	@FXML
+	private Label checkinError;
+	@FXML
+	private TextArea prevCheckin;
 	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	private String destination;
+	private String username;
 	
 	public DoctorPortalController()
 	{
@@ -66,17 +76,61 @@ public class DoctorPortalController{
 	public void search(ActionEvent event) throws IOException
 	{
 		System.out.println("search");
+		try {
+			Database db = new Database();
+			Connection c = db.connect();
+			Statement stmt = c.createStatement();
+			
+			// search the database for shopping_cart with matching userID
+			String sql = "SELECT * FROM patient WHERE (first_name = '" + firstName.getText().toString() +
+												  "' AND last_name = '" + lastName.getText().toString() + "'"
+												  		+ "AND dob = '" + dateOfBirth.getText().toString()+"');";	
+			ResultSet rs = stmt.executeQuery(sql); 
+			
+			if(rs.next()) {
+				String first = rs.getString("first_name");
+				String last = rs.getString("last_name"); 
+				username = rs.getString("username");
+				// ************** TODO
+				System.out.println(first);
+				System.out.println(last);
+				searchError.setText("");
+				displayLastCheckin();
+			} else {
+				searchError.setText("Patient not found.");
+			}
+				
+		} catch (SQLException e) {	
+			System.out.println("Error in connecting to postgreSQL server.");
+			e.printStackTrace();
+		}
 	}
 	
 	public void beginExam(ActionEvent event) throws IOException
 	{
-		System.out.println("checkin");
-		destination = "PatientCheckIn.fxml";    //testing
-		root = FXMLLoader.load(getClass().getResource(destination));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+		//TODO check if username is selected (should be assigned from search function).
+		if(username == null || username == "") {
+			checkinError.setText("Oops, no patients selected.");
+		}else {
+			System.out.println("checkin");
+			destination = "PatientExam.fxml";    //testing
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
+			root = loader.load();
+			
+			PatientExamController patientExamController = loader.getController();
+			patientExamController.setUsername(username);
+			patientExamController.displayPrevCheckIn();
+			
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	
+	public void displayLastCheckin() {
+		DoctorNurseActions actions = new DoctorNurseActions(username);
+		prevCheckin.setText(actions.getLatestCheckin());
 	}
 	
 }
---
+++ src/application/PatientExamController.java
@@ -0,0 +1,120 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+
+public class PatientExamController implements Initializable {
+
+	@FXML
+	private Button signOut;
+	@FXML
+	private Button toMessages;
+	@FXML
+	private Button toHome;
+	@FXML
+	private Button saveInputs;
+	@FXML
+	private TextArea prevCheckIn;
+	@FXML
+	private TextArea inputPrescription;
+	@FXML
+	private TextArea inputNotes;
+	@FXML
+	private Label checkinError;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private String destination;
+	private String prescriptionsData;
+	private String notesData;
+	private String username;
+	
+	public PatientExamController() {
+		
+	}
+	
+	public void displayPrevCheckIn() {
+		DoctorNurseActions action = new DoctorNurseActions(username);
+		String patientDetails = action.getLatestCheckin();
+		prevCheckIn.setText(patientDetails);
+	}
+	
+	public void signOut(ActionEvent event) throws IOException
+	{	
+		destination = "login.fxml";
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void toMessages(ActionEvent event) throws IOException
+	{
+		//destination = "Messages.fxml";
+		destination = "login.fxml";    //testing
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void toHome(ActionEvent event) throws IOException
+	{
+		destination = "DoctorsPortal.fxml"; 
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void saveInputs(ActionEvent event) throws IOException{
+		prescriptionsData=inputPrescription.getText().toString();
+		notesData=inputNotes.getText().toString();
+		
+		if(isFieldEmpty(prescriptionsData,notesData))
+			checkinError.setText("Oops, you missed some required fields.");
+		
+		else{
+			saveEntry();
+			// May change destination later
+			toHome(event);
+		}
+	}
+	
+	public void saveEntry() {
+		
+	}
+	
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public static boolean isFieldEmpty(String... strings) {
+		for(String s : strings) 
+			if(s == null || s.isEmpty()) 
+				return true;
+				
+		return false;
+	}
+}
