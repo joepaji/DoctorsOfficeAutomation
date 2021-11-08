8d5dbc12af78aada197b0668ddf227006594628d
Tobin Wieland <tobin.wieland@gmail.com>
2021-11-08 11:44:03 -0700

    cass for confirmation created as well as the linking of the protals to eachother

---
+++ src/application/patientCheckinConfirmation.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+
+public class patientCheckinConfirmation {
+
+	@FXML
+	private Button confirm;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private String destination;
+	//private String username;
+	
+	public void backToNursePortal(ActionEvent event) throws IOException {
+		destination = "NursePortal.fxml";
+		root = FXMLLoader.load(getClass().getResource(destination));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
