f9fd72b23e03e78146ceda9f10fceba095e7bf56
George Isayev <gisayev0357@gmail.com>
2021-11-15 18:54:15 -0700

    Additional notes & other

--- src/application/PatientEditController.java
+++ src/application/PatientEditController.java
@@ -1,5 +1,6 @@
 package application;
 
+//Importing needed libraries
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -45,7 +46,7 @@ public class PatientEditController {
 	@FXML
 	private Label errorMessage;
 	
-	//Variables
+	//Creating Variables
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
@@ -57,20 +58,27 @@ public class PatientEditController {
 
 	}
 	
+	//Method to set the username 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	
+	
 	//Code for buttons
 	//Home button for the patient
 	public void patientHome(ActionEvent event) throws IOException {
+		////Making the destination the patient portal GUI
 		destination = "PatientPortal.fxml";   
+		
+		//Creating a new FXMLLoader to be able to set the usename on the click of the button
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
 		root = loader.load();
 		PatientPortalController ppc = loader.getController();
 		ppc.setUsername(username);
 		ppc.lastVisitSummary();
 		ppc.patientContactinfo();
+		
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -79,7 +87,10 @@ public class PatientEditController {
 		
 	//Visits button for the patient
 	public void patientVisits(ActionEvent event) throws IOException {
+		//Making the destination the visits GUI
 		destination = "Visits.fxml";
+		
+		//Creating the stage and scene of the GUI to go to the visits page
 		root = FXMLLoader.load(getClass().getResource(destination));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  
 		scene = new Scene(root);
@@ -89,7 +100,10 @@ public class PatientEditController {
 		
 	//Messages button for the patients
 	public void patientMessages(ActionEvent event) throws IOException {
+		//Making the destination the messages GUI
 		destination = "Messages.fxml";
+		
+		//Creating the stage and scene of the GUI to go to the visits page
 		root = FXMLLoader.load(getClass().getResource(destination));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
 		scene = new Scene(root);
@@ -99,7 +113,10 @@ public class PatientEditController {
 		
 	//Sign out button for the patient
 	public void patientSignOut(ActionEvent event) throws IOException {
+		//Making the destination the login GUI 
 		destination = "login.fxml";
+		
+		//Creating the stage and scene of the GUI to go to the login page
 		root = FXMLLoader.load(getClass().getResource(destination));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -109,7 +126,7 @@ public class PatientEditController {
 	
 	//Save Button for the patient
 	public void patientSave(ActionEvent event) throws IOException {
-		//If statment to check for errors
+		//If statment to check all the textfields are not empty
 		if (fNameEdit.getText().isEmpty()) {
 			errorMessage.setText("Error! Please Fill All Boxes");
 		}
@@ -126,15 +143,21 @@ public class PatientEditController {
 			errorMessage.setText("Error! Please Fill All Boxes");
 		}
 		else {
+			//Calling method to save in database
 			saveEdit();
+			
+			//Making the destination the patient portal GUI 
 			destination = "PatientPortal.fxml"; 
+			
+			//Creating a new FXMLLoader to be able to change the edits on the click of the button
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
 			root = loader.load();
 			PatientPortalController ppc = loader.getController();
 			ppc.setUsername(username);
 			ppc.lastVisitSummary();
 			ppc.patientContactinfo();
-			//root = FXMLLoader.load(getClass().getResource(destination));
+			
+			//Creating the stage and scene of the GUI to go to the patient portal page
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
 			scene = new Scene(root);
 			stage.setScene(scene);
@@ -145,43 +168,28 @@ public class PatientEditController {
 	//patientSave method
 	public void saveEdit() {
 		try {
+			//Creating vriables to save from the database
 			String fName = fNameEdit.getText().toString();
 			String lName = lNameEdit.getText().toString();
 			String dob = dobEdit.getText().toString();
 			String phoneNum = phoneNumEdit.getText().toString();
 			String emailAddress = emailAddressEdit.getText().toString();
 			
+			//Connecting to the database
 			Database db = new Database();
 			Connection c = db.connect();	
+			
+			//Query to update the edits 
 			String sql ="UPDATE patient SET first_name='"+fName+"',last_name='"+lName+"',dob='"+dob+"',phone='"+phoneNum+"',email='"+emailAddress+"' WHERE username='"+username+"'";
 			PreparedStatement prepStatement = c.prepareStatement(sql);
 			prepStatement.executeUpdate();
 			c.close();
 		} catch (SQLException e) {	
+			
+			//Error message if it catches an exception
 			System.out.println("Error in adding record. Check that all required values are filled out.");
 			e.printStackTrace();
 		}
 		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 }
--- src/application/PatientPortalController.java
+++ src/application/PatientPortalController.java
@@ -1,5 +1,6 @@
 package application;
 
+//Importing needed libraries
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -40,7 +41,7 @@ public class PatientPortalController implements Initializable{
 	@FXML
 	private TextArea lastVisitSummary;
 	
-	//Variables
+	//Creating Variables
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
@@ -52,6 +53,7 @@ public class PatientPortalController implements Initializable{
 
 	}
 	
+	//Method to set the username 
 	public void setUsername(String username) {
 		this.username = username;
 	}
@@ -65,29 +67,36 @@ public class PatientPortalController implements Initializable{
 	//Code for text areas
 	//Shows patient's last visit summary
 	public void lastVisitSummary() {
+		//Setting the textfield with the given username
 		DoctorNurseActions actions = new DoctorNurseActions(username);
 		lastVisitSummary.setText(actions.getLatestCheckin());
 	}
 	
 	//Shows patient's contact summary
 	public void patientContactinfo() {
+		//Creating variables
 		String patientInfo = "";
 		String fName, lName, dob, phoneNum, emailAddress;
 		
 		//PostGreSQL
 		try {
+			//Query line to get variables
 			String sql = "SELECT first_name, last_name, dob, phone, email "
 					+ " FROM patient"
 					+ " WHERE username = '" + username + "';";
 			
+			//Connecting to dataabse
 			Database database = new Database();
 			Connection c = database.connect();
 			Statement stmt = c.createStatement(
 				    ResultSet.TYPE_SCROLL_INSENSITIVE,
 				    ResultSet.CONCUR_READ_ONLY
 				);
+			
+			//Executing the statment
 			ResultSet result = stmt.executeQuery(sql);
 			
+			//If statment to get variables
 			if(result.first()) {
 				// Get contact information
 				fName = result.getString("first_name");
@@ -107,9 +116,11 @@ public class PatientPortalController implements Initializable{
 			c.close();
 		}
 		catch(SQLException e) {
+			//Catches error 
 			e.printStackTrace();
 		}
 	
+		//Setting the textfield
 		contactInfo.setText(patientInfo);
 	}
 	
@@ -117,13 +128,18 @@ public class PatientPortalController implements Initializable{
 	//Code for buttons
 	//Home button for the patient
 	public void patientHome(ActionEvent event) throws IOException {
-		destination = "PatientPortal.fxml";   
+		//Making the destination the patient portal GUI
+		destination = "PatientPortal.fxml";
+		
+		//Creating a new FXMLLoader to be able to set the usename on the click of the button and text fields to load
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
 		root = loader.load();
 		PatientPortalController ppc = loader.getController();
 		ppc.setUsername(username);
 		ppc.lastVisitSummary();
 		ppc.patientContactinfo();
+		
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -132,13 +148,17 @@ public class PatientPortalController implements Initializable{
 	
 	//Visits button for the patient
 	public void patientVisits(ActionEvent event) throws IOException {
+		//Making the destination the visits GUI
 		destination = "Visits.fxml";
+		
+		//Creating a new FXMLLoader to be able to load the visits on button press passing the username
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
 		root = loader.load();
 		VisitsController vc = loader.getController();
 		vc.setUsername(username);
 		vc.allVisitSummary();
 	
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();  
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -147,8 +167,11 @@ public class PatientPortalController implements Initializable{
 	
 	//Messages button for the patients
 	public void patientMessages(ActionEvent event) throws IOException {
+		//Making the destination the messages GUI
 		destination = "Messages.fxml";
 		root = FXMLLoader.load(getClass().getResource(destination));
+		
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -157,8 +180,11 @@ public class PatientPortalController implements Initializable{
 	
 	//Sign out button for the patient
 	public void patientSignOut(ActionEvent event) throws IOException {
+		//Making the destination the login GUI
 		destination = "login.fxml";
 		root = FXMLLoader.load(getClass().getResource(destination));
+		
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -167,12 +193,16 @@ public class PatientPortalController implements Initializable{
 	
 	//Edit button to edit patient info
 	public void patientEdit(ActionEvent event) throws IOException {
+		//Making the destination the patient contact edit GUI
 		destination = "patientContactEdit.fxml";
+		
+		//Creating a new FXMLLoader to be able to load the edits on button press
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(destination));
-		//root = FXMLLoader.load(getClass().getResource(destination));
 		root = loader.load();
 		PatientEditController pec = loader.getController();
 		pec.setUsername(username);
+		
+		//Creating the stage and scene of the GUI
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
