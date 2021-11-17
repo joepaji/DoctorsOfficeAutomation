fe820b5058e5ee90a43bccbe0ff98e57a9a499f8
kase lunt <kllunt@asu.edu>
2021-11-16 21:08:23 -0700

    fixed the messages bug that was making us choose between displaying new messages at the bottom of the list or missing the most recent message. It now displays the most recent message sent as soon as the user sends it, and the top of the list. Also completed the commenting of the messengerPortal

--- src/application/MessengerController.java
+++ src/application/MessengerController.java
@@ -62,6 +62,7 @@ public class MessengerController implements Initializable{
 		
 	}
 	
+	// Sign the user out, return to login 
 	public void signOut(ActionEvent event) throws IOException
 	{	
 		destination = "login.fxml";
@@ -72,101 +73,102 @@ public class MessengerController implements Initializable{
 		stage.show();
 	}
 	
-	// to home? need to distinguish between who is going where
+	// Takes the user back to the appropriate home portals
 	public void toHome(ActionEvent event) throws IOException
 	{
 		
-		int loginType=-1;
+		int loginType = -1;
 		
+		// Create the query 
 		String sql="SELECT usertype FROM patient WHERE username='" + username + "'"
 				+ " union SELECT usertype FROM staff WHERE username='" + username + "';";
 		
+		// Connect to the database
 		Database database = new Database();
 		Connection c = database.connect();
 		Statement stmt;
+		
 		try {
 			stmt = c.createStatement(
-				    ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
 				    ResultSet.CONCUR_READ_ONLY);
-				
-		ResultSet result = stmt.executeQuery(sql);
-		if(result.next()) {
-			loginType=result.getInt("usertype");
-		}
+			
+			// Execute the statement and save output into result		
+			ResultSet result = stmt.executeQuery(sql);
+		
+			// Set the usertype data into loginType
+			if(result.next())
+			{
+				loginType=result.getInt("usertype");
+			}
 					
 		
-		switch(loginType) // this switch will determine what the outcome of the authenticator was
-		{
+			switch(loginType) // Set the appropriate destination based on the usertype that was authenticated
+			{
 			
-			case 1 : destination = "PatientPortal.fxml";  
+				case 1 : destination = "PatientPortal.fxml";  
 			
-				//This passes the username of the patient to the patientCheckIn controller
-				FXMLLoader patientLoader = new FXMLLoader(getClass().getResource(destination));
-				root = patientLoader.load();
+					//This passes the username of the patient to the patientCheckIn controller
+					FXMLLoader patientLoader = new FXMLLoader(getClass().getResource(destination));
+					root = patientLoader.load();
 				
-				PatientPortalController patientPortal = patientLoader.getController();
-				patientPortal.setUsername(username);
-				patientPortal.lastVisitSummary();
-				patientPortal.patientContactinfo();
+					PatientPortalController patientPortal = patientLoader.getController();
+					patientPortal.setUsername(username);
+					patientPortal.lastVisitSummary();
+					patientPortal.patientContactinfo();
 
 
-				break;
+					break;
 				
-			case 2 : destination = "NursePortal.fxml";	
+				case 2 : destination = "NursePortal.fxml";	
 			
-				//This passes the username of the patient to the patientCheckIn controller
-				FXMLLoader nurseLoader = new FXMLLoader(getClass().getResource(destination));
-				root = nurseLoader.load();
+					//This passes the username of the patient to the patientCheckIn controller
+					FXMLLoader nurseLoader = new FXMLLoader(getClass().getResource(destination));
+					root = nurseLoader.load();
 			
-				NursePortalController nursePortal = nurseLoader.getController();
-				nursePortal.setUsername(username);
+					NursePortalController nursePortal = nurseLoader.getController();
+					nursePortal.setUsername(username);
 				
-				break;
+					break;
 				
-			case 3 : destination = "DoctorPortal.fxml";
+				case 3 : destination = "DoctorPortal.fxml";
 				
-				//This passes the username of the patient to the patientCheckIn controller
-				FXMLLoader doctorLoader = new FXMLLoader(getClass().getResource(destination));
-				root = doctorLoader.load();
-		
-				DoctorPortalController doctorPortal = doctorLoader.getController();
-				doctorPortal.setUsername(username);
+					// Passes the username of the patient to the patientCheckIn controller
+					FXMLLoader doctorLoader = new FXMLLoader(getClass().getResource(destination));
+					root = doctorLoader.load();
+					DoctorPortalController doctorPortal = doctorLoader.getController();
+					doctorPortal.setUsername(username);
 			
-				break;				
-		}
+					break;				
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
-		if(destination != "")  // make sure there was a valid destination
+		if(destination != "")  // Check for valid destination, and then set and show new scene
 		{	
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();  // assigns the stage to the currently running stage from main
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();  
 			scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();			
 		}
-		/*
-		 * destination = "PatientPortal.fxml"; root =
-		 * FXMLLoader.load(getClass().getResource(destination)); stage =
-		 * (Stage)((Node)event.getSource()).getScene().getWindow(); // assigns the stage
-		 * to the currently running stage from main scene = new Scene(root);
-		 * stage.setScene(scene); stage.show();
-		 */
 	}
 	
 	public void sendMessage(ActionEvent event) throws IOException
 	{
-		
+		// Retrieve the text data from text fields
 		recipientFirstName = recipientFirst.getText().toString();
 		recipientLastName = recipientLast.getText().toString();
 		dateString = date.getText().toString();
 		message = messageBody.getText().toString();
+		
 		try {
+			// Create the query 
 			String sql = "INSERT INTO messages(receiver_first, receiver_last, sender_first, sender_last, date, message_body)"
 				 	   + "VALUES('"+ recipientFirstName +"', '"+ recipientLastName +"', '"+ first +"', '"+ last +"', '"+ dateString +"', '"+ message +"');";
 			
+			// Connect to the database
 			Database database = new Database();
 			Connection c = database.connect();
 			Statement stmt = c.createStatement(
@@ -174,11 +176,11 @@ public class MessengerController implements Initializable{
 				    ResultSet.CONCUR_READ_ONLY
 			);
 			
+			// Execute the update and display messages again
 			stmt.executeUpdate(sql);
-			
 			displayMessages();
 			
-			
+			// Close connection
 			c.close();
 		}
 		catch(SQLException e) {
@@ -186,23 +188,35 @@ public class MessengerController implements Initializable{
 		}
 	}
 	
+	// Display the current user's messages in the database
 	public void displayMessages()
 	{
 		try {
+			// Create the query by searching for messages to or from current user
 			String sql = "SELECT * FROM messages WHERE ( (receiver_first = '"+ first +"' and receiver_last = '"+ last +"') or "
-													  + "(sender_first = '"+ first +"' and sender_last = '"+ last +"') );";
+					   + "(sender_first = '"+ first +"' and sender_last = '"+ last +"') );";
+			
+			// Connect to the database
 			Database database = new Database();
 			Connection c = database.connect();
 			Statement stmt = c.createStatement(
 				    ResultSet.TYPE_SCROLL_INSENSITIVE,
 				    ResultSet.CONCUR_READ_ONLY
 			);
-			
+			// Execute query and save the output into result
 			ResultSet result = stmt.executeQuery(sql);
 			String output = "";
 			
+			// Move the resultset cursor to the last row and retrieve the appropriate column data
+			result.last();
+			output += "Sender:\t" + result.getString(3) + " " + result.getString(4) + "\n";
+			output += "Recipient:\t" + result.getString(5) + " " + result.getString(6) + "\n";
+			output += "Date:\t\t" + result.getString(2) + "\n\n";
+			output += "Message:\t" + result.getString(1) + "\n\n";
+			output += "---------------------------------------------------------\n";
 			
-			while(result.next())
+			// Move cursor to the previous entry. While result is not empty, iterate through and add to the formatted string
+			while(result.previous())
 			{
 				output += "Sender:\t" + result.getString(3) + " " + result.getString(4) + "\n";
 				output += "Recipient:\t" + result.getString(5) + " " + result.getString(6) + "\n";
@@ -210,11 +224,13 @@ public class MessengerController implements Initializable{
 				output += "Message:\t" + result.getString(1) + "\n\n";
 				output += "---------------------------------------------------------\n";
 				
-			}
+			}	
+		
+			// Load the created string into the recentMessages textArea
 			recentMessages.setText(output);
 			
 			
-			
+			// Close the connection
 			c.close();
 		}
 		catch(SQLException e) {
@@ -222,12 +238,15 @@ public class MessengerController implements Initializable{
 		}
 	}
 	
+	// Set the first and last name of the user by searching the database by username
 	public void setSelfFirstLast()
 	{
 		try {
+			// Create the query
 			String sql = "SELECT first_name, last_name FROM staff WHERE (username = '"+ username +"')"
 					+ "union SELECT first_name, last_name FROM patient WHERE (username = '"+ username +"');";
 			
+			// Connect to the database
 			Database database = new Database();
 			Connection c = database.connect();
 			Statement stmt = c.createStatement(
@@ -235,15 +254,16 @@ public class MessengerController implements Initializable{
 				    ResultSet.CONCUR_READ_ONLY
 			);
 			
+			// Execute query and save output into result
 			ResultSet result = stmt.executeQuery(sql);
 			
+			// If result is not null, set this.first and this.last
 			if(result.next())
 			{
 				this.first = result.getString("first_name");
 				this.last = result.getString("last_name");
 			}
-			
-			
+			// Close the connection
 			c.close();
 		}
 		catch(SQLException e) {
@@ -251,6 +271,7 @@ public class MessengerController implements Initializable{
 		}
 	}
 	
+	// Set the username of the user. Used to pass user data from previous controller
 	public void setUsername(String username) {
 		this.username = username;
 	}
